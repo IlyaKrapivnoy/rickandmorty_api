b326978abcc5fd4a9b504059f7751d6f0ca0acc5
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-16 01:13:18 +0200

    add dynamic Layout

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -3,6 +3,7 @@ import imageLoader from "../../imageLoader";
 import { useRouter } from "next/router";
 import { Character } from "../../types";
 import { GetServerSideProps } from "next";
+import Layout from "../../components/Layout";
 
 function CharacterPage({ character }: { character: Character }) {
   const router = useRouter();
@@ -26,6 +27,10 @@ function CharacterPage({ character }: { character: Character }) {
     </div>
   );
 }
+
+CharacterPage.getLayout = function getLayout(page: typeof CharacterPage) {
+  return <Layout>{page}</Layout>;
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const res = await fetch(
